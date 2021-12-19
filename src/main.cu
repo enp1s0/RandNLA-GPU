692e31c3a0042d06d2832250b002073314de4165
mutsuki <mutsuki@momo86.net>
2021-12-19 15:14:23 +0900

    Fix test input

--- src/main.cu
+++ src/main.cu
@@ -24,15 +24,15 @@ void evaluate(
 	const auto A_size = rsvd.get_m() * rsvd.get_n();
 	const auto S_size = std::min(rsvd.get_m(), rsvd.get_n());
 	const auto U_size = rsvd.get_m() * (rsvd.get_k() + rsvd.get_p());
-	const auto Vt_size = rsvd.get_n() * (rsvd.get_k() + rsvd.get_p());
+	const auto V_size = rsvd.get_n() * (rsvd.get_k() + rsvd.get_p());
 
 	auto A_uptr = cutf::memory::get_device_unique_ptr<float>(A_size);
 	auto U_uptr = cutf::memory::get_device_unique_ptr<float>(U_size);
 	auto S_uptr = cutf::memory::get_device_unique_ptr<float>(S_size);
-	auto Vt_uptr = cutf::memory::get_device_unique_ptr<float>(Vt_size);
+	auto V_uptr = cutf::memory::get_device_unique_ptr<float>(V_size);
 
 	rsvd.set_input_ptr(A_uptr.get());
-	rsvd.set_output_ptr(U_uptr.get(), S_uptr.get(), Vt_uptr.get());
+	rsvd.set_output_ptr(U_uptr.get(), S_uptr.get(), V_uptr.get());
 
 	rsvd.prepare();
 
@@ -50,7 +50,7 @@ int main() {
 	for (unsigned log_m = 5; log_m <= max_log_m; log_m++) {
 		for (unsigned log_n = 5; log_n <= max_log_n; log_n++) {
 			const auto max_log_k = std::min(log_m, log_n);
-			for (unsigned log_k = 4; max_log_k - 1; log_k++) {
+			for (unsigned log_k = 4; log_k <= max_log_k - 1; log_k++) {
 				const auto m = 1u << log_m;
 				const auto n = 1u << log_n;
 				const auto k = 1u << log_k;
@@ -66,7 +66,7 @@ int main() {
 						nullptr, m,
 						nullptr, m,
 						nullptr,
-						nullptr, k + p
+						nullptr, n
 						);
 
 				evaluate("cusolver", rsvd_cusolver, 10);
