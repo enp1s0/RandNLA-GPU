1ec287e0abec4faefb09823b5a12de8a21e2e696
mutsuki <mutsuki@momo86.net>
2021-12-21 22:25:56 +0900

    Add the declaration of `get_input_matrix`

--- src/input_matrix.hpp
+++ src/input_matrix.hpp
@@ -1,8 +1,17 @@
 #ifndef __RSVD_TEST_INPUT_MATRIX_HPP__
 #define __RSVD_TEST_INPUT_MATRIX_HPP__
+#include <string>
+
 namespace mtk {
 namespace rsvd_test {
-
+template <class T>
+void get_input_matrix(
+		T* const ptr,
+		const std::string input_matrix_name,
+		const std::size_t m,
+		const std::size_t n,
+		const std::uint64_t seed
+		);
 } // namespace rsvd_test
 } // namespace mtk
 #endif
