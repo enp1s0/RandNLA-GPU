bea7fe410160186324a7ad2a28de3b849d1c9413
mutsuki <mutsuki@momo86.net>
2022-02-08 13:06:46 +0900

    Add the declaration of `svd_jaccobi`

--- src/svd_base.hpp
+++ src/svd_base.hpp
@@ -1,6 +1,7 @@
 #ifndef __MP_RSVD_SVD_BASE_HPP__
 #define __MP_RSVD_SVD_BASE_HPP__
 #include <cstdint>
+#include <string>
 #include <cusolver.h>
 
 namespace mtk {
@@ -12,11 +13,16 @@ protected:
 	std::size_t work_size;
 
 	cusolverDnHandle_t const cusolver_handle;
+
+	const std::string name;
 public:
 	svd_base(
+			const std::string name,
 			const std::size_t m, const std::size_t n,
 			cusolverDnHandle_t const cusolver_handle
-			) : m(m), n(n), cusolver_handle(cusolver_handle) {}
+			) : name(name), m(m), n(n), cusolver_handle(cusolver_handle) {}
+
+	std::string get_name_str() const {return name;};
 
 	virtual void run(
 		float* const S_ptr,
@@ -27,6 +33,8 @@ public:
 	virtual std::size_t get_working_mem_size_in_byte() = 0;
 
 	virtual char op_v() const = 0;
+
+	virtual void preprocess();
 };
 
 struct svd_qr : public svd_base {
@@ -34,7 +42,7 @@ public:
 	svd_qr(
 			const std::size_t m, const std::size_t n,
 			cusolverDnHandle_t const cusolver_handle)
-		: svd_base(m, n, cusolver_handle) {}
+		: svd_base("svd_qr", m, n, cusolver_handle) {}
 
 	void run(
 		float* const S_ptr,
@@ -45,6 +53,26 @@ public:
 	std::size_t get_working_mem_size_in_byte();
 
 	char op_v() const {return 'T';}
+	void preprocess() {};
+};
+
+struct svd_jaccobi : public svd_base {
+public:
+	svd_jaccobi(
+			const std::size_t m, const std::size_t n,
+			cusolverDnHandle_t const cusolver_handle)
+		: svd_base("svd_jaccobi", m, n, cusolver_handle) {}
+
+	void run(
+		float* const S_ptr,
+		float* const U_ptr, const std::size_t ldu,
+		float* const V_ptr, const std::size_t ldv,
+		float* const input_ptr, const std::size_t ld,
+		float* const work_ptr);
+	std::size_t get_working_mem_size_in_byte();
+
+	char op_v() const {return 'N';}
+	void preprocess();
 };
 } // rsvd_test
 } // mtk
