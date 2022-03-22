1215ade6569e40ef60dc0e4185ad1715d9e10e7a
mutsuki <mutsuki@momo86.net>
2022-03-22 23:27:25 +0900

    Add gen_latms_sigmoid_matrix

--- rsvd/src/input_matrix.hpp
+++ rsvd/src/input_matrix.hpp
@@ -28,6 +28,14 @@ void gen_latms_matrix(
 		const std::size_t rank,
 		const std::uint64_t seed
 		);
+void gen_latms_sigmoid_matrix(
+		float* const ptr,
+		const std::size_t ld,
+		const std::size_t m,
+		const std::size_t n,
+		const std::size_t p,
+		const std::uint64_t seed
+		);
 } // namespace rsvd_test
 } // namespace mtk
 #endif
